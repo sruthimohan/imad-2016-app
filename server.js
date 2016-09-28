93846f642828525edd34291b5a090022a4802d39
sruthimohan <sruthim2907@gmail.com>
2016-09-28 15:00:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/download.jpg', function (req, res) {
 
 app.get('/article-one',function(req,res){
     
-   res.send('Article one is served here'); 
+   res.sendFile(path.join(_dirname,'ui','article-one.html')); 
 });
 app.get('/article-two',function(req,res){
     
